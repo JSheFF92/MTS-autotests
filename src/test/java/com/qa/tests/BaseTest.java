e4a2f96723b2b84ff94570f1694a7c69c60765ab
Evgenyi <shevchuk291992@gmail.com>
2023-09-24 21:53:06 +0300

    delete comment

--- src/test/java/com/qa/tests/BaseTest.java
+++ src/test/java/com/qa/tests/BaseTest.java
@@ -1,6 +1,5 @@
 package com.qa.tests;
 
-import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import com.qa.config.ConfigReader;
 import com.qa.config.WebConfig;
@@ -11,9 +10,6 @@ import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
-import org.openqa.selenium.remote.DesiredCapabilities;
-
-import java.util.Map;
 
 import static com.codeborne.selenide.Selenide.*;
 
@@ -29,25 +25,6 @@ public class BaseTest {
         webConfigForProject.webConfig();
 
     }
-
-//    @BeforeAll
-//    static void beforeAll() {
-//        Configuration.browser = System.getProperty("browser", "chrome");
-//        Configuration.baseUrl = System.getProperty("baseUrl", "https://moskva.mts.ru");
-//        Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
-//        Configuration.browserVersion = System.getProperty("browserVersion", "100.0");
-//        Configuration.pageLoadStrategy = "eager";
-//        Configuration.timeout = 10000;
-//        Configuration.remote = System.getProperty("selenoidBaseUrl", "https://user1:1234@selenoid.autotests.cloud/wd/hub");
-//
-//        DesiredCapabilities capabilities = new DesiredCapabilities();
-//        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
-//                "enableVNC", true,
-//                "enableVideo", true
-//        ));
-//
-//        Configuration.browserCapabilities = capabilities;
-//    }
 
     @BeforeEach
     void addListener() {
